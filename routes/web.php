2642f3cc5521def61bd939c67f59fb1a9c0e2c0e
Sergio <salossetti@teco.com.ar>
2025-05-15 14:31:18 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth']) // ajustable
-    ->prefix('admin')
-    ->name('admin.')
+Route::middleware(['web', 'auth'])
     ->group(function () {
-        Route::get('/menu-editor', \MenuEditor\Livewire\MenuManager::class)
-            ->name('menu-editor');
+        Route::get('/menu-editor', function () {
+            return view('menu-editor::page');
+        })->name('menu-editor');
     });
+
