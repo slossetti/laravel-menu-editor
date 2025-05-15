cb45e3bfff90e5677891ec35931e568f44a9fc4f
Sergio <salossetti@teco.com.ar>
2025-05-15 14:41:18 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth'])
-    ->group(function () {
-        Route::get('/menu-editor', function () {
-            return view('menu-editor::page');
-        })->name('menu-editor');
-    });
-
+Route::get('/menu-editor', function () {
+    return view('menu-editor::page');
+})->name('menu-editor');
